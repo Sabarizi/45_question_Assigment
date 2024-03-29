e33e63278dbae2dd0afe4e46eead406258e5f4d0
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:18:24 +0500

    Add files via upload

---
+++ exercise-21/main.js
@@ -0,0 +1,15 @@
+//person object
+var person = {
+    name: 'Rizwan',
+    age: 30,
+    nationality: 'pakistani',
+    student: 'true'
+};
+console.log(person);
+var car = {
+    maker: 'Toyota',
+    color: 'Black',
+    automatic: 'true',
+    model: ' 2024',
+};
+console.log(car);
---
+++ exercise-21/main.ts
@@ -0,0 +1,33 @@
+//datatype of person
+interface person{
+    name: string,
+    age: number,
+    nationality: string,
+    student :boolean,
+}
+
+//person object
+let person ={
+    name: 'Rizwan',
+    age : 30,
+    nationality : 'pakistani',
+    student : 'true'
+}
+console.log(person);
+//datatype of car 
+interface car{
+    maker : string
+    color : string
+    automatic : boolean
+    model : number
+}
+
+let car ={
+    maker :'Toyota',
+    color : 'Black',
+    automatic :'true',
+    model : ' 2024',
+}
+
+console.log(car);
+

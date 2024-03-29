8074266057d17fcd7464f4a9499a3d89fe5a3c4b
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 21:27:59 +0500

    Add files via upload

---
+++ exercise-07/main.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(6 + 2); //addition
+console.log(10 - 2); //sub
+console.log(4 * 2); // multiply
+console.log(16 / 2); // divison
---
+++ exercise-07/main.ts
@@ -0,0 +1,8 @@
+"use strict";
+console.log(6+2); //addition
+
+console.log(10-2); //sub
+
+console.log(4*2); // multiply
+
+console.log(16/2); // divison

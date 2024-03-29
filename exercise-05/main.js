eb02c4dd7b8d60a8cc9ac11497fbbf834736e3b3
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 21:24:08 +0500

    Add files via upload

---
+++ exercise-05/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'William Shakespeare';
+let quote = "All that glitter is not gold";
+console.log(`${famous_person} once said,${quote}`);
---
+++ exercise-05/main.ts
@@ -0,0 +1,3 @@
+let famous_person :string = 'William Shakespeare';
+let quote  :string ="All that glitter is not gold";
+console.log(`${famous_person} once said,${quote}`);

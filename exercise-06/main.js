8e22247ad944c02af6024a8f71d872a817052894
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 21:26:10 +0500

    Add files via upload

---
+++ exercise-06/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const personName = '    \n\t Nuzhat Saba \t\n    ';
+console.log(personName);
+const without_whitespace = personName.trim();
+console.log(without_whitespace);
---
+++ exercise-06/main.ts
@@ -0,0 +1,5 @@
+const personName :string ='    \n\t Nuzhat Saba \t\n    ';
+console.log(personName);
+
+const without_whitespace :string = personName.trim();
+console.log(without_whitespace);

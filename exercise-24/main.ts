8749f5cc702f3275409db557e95df0d3e0e97ea0
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:24:27 +0500

    Add files via upload

---
+++ exercise-24/main.ts
@@ -0,0 +1,24 @@
+//Equality and inequality test 1
+console.log("Equality test with String :" , "Apple" === "Apple") ;
+
+//Equality and inequality test 2
+console.log("Inequality test with String :" , ("Apple" as string ) === "orange") ;
+
+// test using lower case function
+console.log("lower case function test:" , "HELLO".toLowerCase()=== "hello");
+
+
+//Numarical test  involving equalty
+console.log("Equality test with number:", 27 === 27);
+
+//Numarical test involving and inequality
+console.log("Inequality test with number:", ( 27 as number) === 29);
+
+//greater than and less than
+console.log("greater than test", 10 > 5);
+
+//greater than or equal to
+console.log("Greater than and equal to test:", 10 >= 10);
+
+//less than or equal to
+console.log("less than or equal to test:", 5 <= 10);

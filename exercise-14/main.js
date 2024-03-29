3f14f258777951f37e64c9b44b0321d5fff82536
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:05:45 +0500

    Add files via upload

---
+++ exercise-14/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Kamran Tessori', 'Zia Khan', 'Daniyal Nagori'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr.' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!');
+}

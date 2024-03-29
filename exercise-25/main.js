ce325109c38e8f906698bcd8d4d9b5fd197a0789
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:26:23 +0500

    Add files via upload

---
+++ exercise-25/main.js
@@ -0,0 +1,10 @@
+//create a variable called alien color
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("players just earn 5 points !");
+}
+//write one version of this program that another fail.
+alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log("players just earn 5 points !");
+}
---
+++ exercise-25/main.ts
@@ -0,0 +1,11 @@
+//create a variable called alien color
+let alien_color :string = 'green';
+    if(alien_color === 'green'){
+        console.log("players just earn 5 points !");
+    }
+//write one version of this program that another fail.
+alien_color = 'yellow';
+if(alien_color === 'green'){
+    console.log("players just earn 5 points !");
+}
+

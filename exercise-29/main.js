6cfdd9720ac49ea00214e18d080adfd77f975542
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:31:28 +0500

    Add files via upload

---
+++ exercise-29/main.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//make arry of your favourite furite and then write a series
+let favourite_furite = ['apple', 'mango', 'banana'];
+//if condition is used for ( which fruite was in aarray that will b print)
+if (favourite_furite.includes('apple')) {
+    console.log('I really like apple');
+}
+if (favourite_furite.includes('banana')) {
+    console.log('I really like banana');
+}
+if (favourite_furite.includes('mango')) {
+    console.log('I really like mango');
+}
+if (favourite_furite.includes('orange')) {
+    console.log('I really like orange');
+}
+if (favourite_furite.includes('pineApple')) {
+    console.log('I really like pineApple');
+}

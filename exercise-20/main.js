085bd86898bd8ef40ecaedbef974c5fb6771d1ca
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:16:30 +0500

    Add files via upload

---
+++ exercise-20/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let items = ['pakistan', 'karachi', 'urdu'];
+console.log(items);
+let item2 = [];
+item2.push('London');
+item2.push('Turkey');
+item2.push('France');
+console.log(item2);
---
+++ exercise-20/main.ts
@@ -0,0 +1,10 @@
+let items :string [] = ['pakistan' ,'karachi' ,'urdu'];
+console.log(items);
+
+let item2 :string[] = [];
+
+item2.push('London');
+item2.push('Turkey');
+item2.push('France');
+
+console.log(item2);

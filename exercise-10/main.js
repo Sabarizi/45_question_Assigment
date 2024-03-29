8115ea9c28427dd5ab95554e928ece78eb4bbabe
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 21:50:48 +0500

    Add files via upload

---
+++ exercise-10/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//program written by Nuzhat Saba
+//current date 20-02-2024
+//this program will write Hello world.
+console.log("Hello World");
---
+++ exercise-10/main2.js
@@ -0,0 +1,5 @@
+"use strict";
+//program is written by Nuzhat Saba
+//current date 20-02-2024
+//This progarm will add two digit.
+console.log("2+2");

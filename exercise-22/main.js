4af73c71c9d0e39ee5fc77654023d00f0c89a139
Sabarizi <160276605+Sabarizi@users.noreply.github.com>
2024-03-29 22:20:07 +0500

    Add files via upload

---
+++ exercise-22/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//creating array
+let newArry = ['quaid e azam', ' allama iqbal', 'liquat ali khan'];
+//accessing array by index
+console.log(newArry[5]);
+//correct the error
+console.log(newArry);
---
+++ exercise-22/main.ts
@@ -0,0 +1,6 @@
+//creating array
+let newArry :string []= ['quaid e azam' , ' allama iqbal', 'liquat ali khan'];
+//accessing array by index
+console.log(newArry[5]);
+//correct the error
+console.log(newArry);
